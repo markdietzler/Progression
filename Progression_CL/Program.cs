e8797cce80ae24e166ed8798c50237b905a8bfed
Mark Dietzler <mdietzler@eagles.ewu.edu>
2020-03-18 10:47:12 -0700

    GUI Update 1

--- Progression_CL/Program.cs
+++ Progression_CL/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Progression_Library.Defaults;
+using System;
+using System.IO;
+using System.Threading;
 
 namespace Progression_CL
 {
@@ -6,7 +9,66 @@ namespace Progression_CL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine(AppDefaults.PoE_LogFile);
+            //File.WriteAllLines("test.txt", new string[] { "XX","YY"});
+
+            new Thread(() => ReadFromFile(AppDefaults.PoE_LogFile, AppDefaults.POE_Logs)).Start();
+
+            //WriteToFile();
         }
+
+        private static void ReadFromFile(string fileToWatch, string folderToWatch)
+        {
+            long _offset = 0;
+            FileSystemWatcher _fsw = new FileSystemWatcher
+            {
+                Path = folderToWatch,
+                Filter = fileToWatch
+            };
+
+            FileStream file = File.Open(
+                fileToWatch,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.Write
+                );
+
+            StreamReader reader = new StreamReader(file);
+            while(true)
+            {
+                _fsw.WaitForChanged(WatcherChangeTypes.Changed);
+                file.Seek(_offset, SeekOrigin.Begin);
+                if(!reader.EndOfStream)
+                {
+                    do
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    } while (!reader.EndOfStream);
+
+                    _offset = file.Position;
+                }
+            }
+        } //end of method
+
+        private static void WriteToFile()
+        {
+            for(int i = 100; i < 100; i++)
+            {
+                FileStream writeFile = File.Open(
+                    "test.txt",
+                    FileMode.Append,
+                    FileAccess.Write,
+                    FileShare.Read
+                    );
+                using(FileStream file = writeFile)
+                {
+                    using(StreamWriter sw = new StreamWriter(file))
+                    {
+                        sw.WriteLine(i);
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+        } //end of method
     }
 }
--- Progression_Library/Defaults/AppDefaults.cs
+++ Progression_Library/Defaults/AppDefaults.cs
@@ -1,8 +1,25 @@
-﻿
+﻿using System;
+
 namespace Progression_Library.Defaults
 {
     public class AppDefaults
     {
+        public static string PoE_LogFile
+        {
+            get
+            {
+                return @"D:\Games\Path of Exile\logs\Client.txt";
+            }
+        }
 
+        public static string POE_Logs
+            
+        {
+            get
+            {
+                return @"D:\Games\Path of Exile\logs\";
+            }
+            
+        }
     }
 }
