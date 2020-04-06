9986c02ce037b7952d61bfb0021bda8829cca8fc
Mark Dietzler <mdietzler@eagles.ewu.edu>
2020-04-06 11:12:20 -0700

    GUI Update 6
    Added basic running window

---
+++ Progression_GUI/ViewModels/RunViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace Progression_GUI.ViewModels
+{
+    public class RunViewModel : ViewModelBase
+    {
+        #region variable declarations
+
+        private bool? _ThreadControl = false;
+
+        public bool? ThreadControl
+        {
+            get => _ThreadControl;
+            set => Set(ref _ThreadControl, value);
+        } 
+
+        #endregion
+
+        public RunViewModel()
+        {
+
+        }
+
+        private void LogFileWatcher()
+        {
+
+        }
+    }
+}
