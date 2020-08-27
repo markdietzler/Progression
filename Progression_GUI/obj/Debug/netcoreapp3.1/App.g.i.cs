846904b99e1c2692b0e78cfc61a94e61d1146a23
Mark Dietzler <mdietzler@eagles.ewu.edu>
2020-08-26 19:12:21 -0700

    class containers for equipment slots

--- Progression_GUI/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Progression_GUI/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -62,7 +62,7 @@ namespace Progression_GUI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Progression_GUI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Progression_GUI;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
